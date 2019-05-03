6c8c29e8b05adc9b3d39633fe86bced959bb5a2c
João Pedro Cardoso <jpcardoso@outlook.pt>
2019-05-03 19:14:37 +0100

    Add Getter tests

--- antcolony/Test.java
+++ antcolony/Test.java
@@ -12,6 +12,9 @@ public class Test {
 
 	
 	public static void test_graphs() {
+	
+			
+		//CREATE A GRAPH
 		graph<String,Double> Gr= new graph<String,Double>();
 		boolean b;
 		b=Gr.addVertex("João");
@@ -19,16 +22,21 @@ public class Test {
 		b=Gr.addVertex("Carolina");
 		b=Gr.addE("João","Matilde",6.5);
 		b=Gr.addE("Carolina","Matilde",3.2);
+		
+		//TEST GRAPH
 		if(b==true)
 			System.out.println(Gr.toString());
 		else System.out.println("Fail");
+		
+		
+		//TEST VERTEX AND EDGE GETTERS
 		Vertex<String,Double> v=Gr.GetVertex(0);
 		Edge<String,Double> e=v.getE().get(0);
 		String label= e.getLabel();
 		Double weight= e.getWeight();
 		System.out.println("Sucess:"+ label + weight);
-				//if (b==true)
-			//System.out.println("it worked:");
-		//else System.out.println("fail");
+		
+		
+		
 	}
 }
